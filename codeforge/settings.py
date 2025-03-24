d0ac479410aaeea1ddd96a9d57cd31ce0961fca2
Gulrez <egulrezalam@gmail.com>
2025-03-24 17:45:13 +0530

    remove unused admin and user apps; add new organization and myapp with landing page

--- codeforge/settings.py
+++ codeforge/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'organization',
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = "codeforge.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
